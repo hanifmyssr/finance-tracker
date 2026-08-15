93343789fe64db2bedabd251b855d8f3f05a80c4
Muhammad Hanif Muyassar <hanifmuyassar@student.upi.edu>
2026-08-15 21:47:56 +0700

    Add files via uploads

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+// Konfigurasi koneksi database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "finance_tracker";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi database gagal: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pencatat Keuangan Pribadi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.php"><i class="fa-solid fa-wallet"></i> Pencatat Keuangan</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navMenu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="transactions.php">Transaksi</a></li>
+                <li class="nav-item"><a class="nav-link" href="add_transaction.php">+ Tambah</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container my-4">
