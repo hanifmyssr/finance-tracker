527b16fd7098b4a7efa88e37be64d3f636a8c8a8
Muhammad Hanif Muyassar <hanifmuyassar@student.upi.edu>
2026-08-17 18:56:35 +0700

    Add theme toggle button to navbar

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script>
+        (function() {
+            const savedTheme = localStorage.getItem('theme') || 'light';
+            document.documentElement.setAttribute('data-theme', savedTheme);
+        })();
+    </script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -16,10 +22,15 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navMenu">
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto align-items-lg-center">
                 <li class="nav-item"><a class="nav-link" href="index.php">Dashboard</a></li>
                 <li class="nav-item"><a class="nav-link" href="transactions.php">Transaksi</a></li>
                 <li class="nav-item"><a class="nav-link" href="add_transaction.php">+ Tambah</a></li>
+                <li class="nav-item ms-lg-2">
+                    <button id="themeToggleBtn" class="theme-toggle-btn">
+                        <i class="fa-solid fa-moon"></i> <span id="themeToggleLabel">Mode Gelap</span>
+                    </button>
+                </li>
             </ul>
         </div>
     </div>
